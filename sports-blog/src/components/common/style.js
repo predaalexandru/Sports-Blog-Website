f656b9523962db35f9163c5cd98cd12141dfb0dc
Preda Alexandru <alexpreda95@yahoo.com>
2020-06-27 15:58:17 +0300

    Style it the posts and making responsive

---
+++ sports-blog/src/components/common/style.js
@@ -0,0 +1,13 @@
+const categoryColors = {
+    'Premier League': 'rgb(255,59,48)',
+    'Champions League': 'rgb(0,113,164)',
+    'NBA': 'rgb(255,45,85)',
+    Fotbal: 'rgb(52,199,89)',
+    Handbal: 'rgb(64,156,255)',
+    Baschet: 'rgb(255,179,64)',
+    Tenis: 'rgb(175,82,250)',
+}
+
+export {
+    categoryColors
+}
